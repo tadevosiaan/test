b334e9b4123eef271cb8b0143bfbba50d97edd57
Arman Tadevosian <tadevosiaan@yandex.ru>
2020-06-03 14:23:00 +0300

    /

--- bioinfo/chapter2_dnapatterns.py
+++ bioinfo/chapter2_dnapatterns.py
@@ -5,6 +5,31 @@ import numpy as np
 # Randomized algorithms
 
 
+def HammingDistance(u: str, v: str) -> int:
+    assert len(u) <= len(v)
+    if len(u) == len(v):
+        dist = 0
+        for i in range(len(u)):
+            if u[i] != v[i]:
+                dist += 1
+    else:
+        dist = float('inf')
+        for i in range(len(v) - len(u) + 1):
+            s = v[i: i + len(u)]
+            d = HammingDistance(s, u)
+            if d < dist:
+                dist = d
+    return dist
+
+
+def d(u: str, DNA: list) -> int:
+    t = len(DNA)
+    dist = 0
+    for i in range(t):
+        dist += HammingDistance(u, DNA[i])
+    return dist
+
+
 def Neighbors(Pattern: str, d: int) -> set:
     # Input: Pattern string, d int
     # Output: d-neighborhood of Pattern in HammDist
@@ -79,30 +104,6 @@ def gen_kMers(k: int) -> list:
         c = list(map(lambda x: 'C' + x, kmers))
         g = list(map(lambda x: 'G' + x, kmers))
         return a + t + c + g
-
-
-def hammdist(u: str, v: str) -> int:
-    assert len(u) <= len(v)
-    if len(u) == len(v):
-        dist = 0
-        for i in range(len(u)):
-            if u[i] != v[i]:
-                dist += 1
-    else:
-        dist = float('inf')
-        for i in range(len(v) - len(u) + 1):
-            s = v[i: i + len(u)]
-            d = hammdist(s, u)
-            if d < dist:
-                dist = d
-    return dist
-
-
-def d(u: str, DNA: list) -> int:
-    t = len(DNA)
-    dist = 0
-    for i in range(t):
-        dist += hammdist(u, DNA[i])
 
 
 def MedianString(DNA: list, k: int) -> str:
